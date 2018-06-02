6763d79594435b28328b702ea6bfd1dd788c0d87
philippguenter <philipp.guenter@golfpost.de>
2018-06-02 08:19:55 +0200

    deleted search form on 404

--- wp-content/themes/radcliffe/404.php
+++ wp-content/themes/radcliffe/404.php
@@ -20,11 +20,7 @@
             <p><?php _e( "It seems like you have tried to open a page that doesn't exist. It could have been deleted, moved, or it never existed at all. You are welcome to search for what you are looking for with the form below.", 'radcliffe' ); ?></p>
             
         </div><!-- .post-content -->
-        
-        <div class="section-inner thin">
-        
-	        <?php get_search_form(); ?>
-        </div>
+
         	            	                        	
 	</div><!-- .post -->
 	
