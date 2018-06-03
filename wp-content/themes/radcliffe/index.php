b4543c89d4bc7f104c5bf595b3e7775cfaadec42
philippguenter <philipp.guenter@golfpost.de>
2018-06-03 10:32:38 +0200

    test

--- wp-content/themes/radcliffe/index.php
+++ wp-content/themes/radcliffe/index.php
@@ -1,7 +1,49 @@
 <?php get_header(); ?>
-<div class="content">
-																	                    
 
+<div class="content">
+
+    <div class="header section light-padding"></div>
+
+        <div class="header-inner section-inner">
+
+
+            <a class="blog-logo" href="https://www.fipsolutions.de/" title="FIPSOLUTIONS — seo | web | design" rel="home">
+                <img src="https://www.fipsolutions.de/wp-content/uploads/2018/05/fipsolutions-logo.png" alt="FIPSOLUTIONS">
+            </a>
+
+
+            <div class="nav-toggle">
+
+                <p>Menü</p>
+
+                <div class="bars">
+
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+
+                    <div class="clear"></div>
+
+                </div>
+
+            </div>
+
+            <ul class="main-menu fright">
+
+                <li id="menu-item-31" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-31"><a href="https://www.fipsolutions.de">Startseite</a></li>
+                <li id="menu-item-44" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-41 current_page_item menu-item-44"><a href="https://www.fipsolutions.de/kontakt">Kontakt</a></li>
+                <li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-30"><a href="https://www.fipsolutions.de/impressum">Impressum</a></li>
+                <li id="menu-item-34" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-34"><a href="https://www.fipsolutions.de/datenschutzerklaerung">Datenschutzerklärung</a></li>
+
+
+
+            </ul>
+
+            <div class="clear"></div>
+
+        </div><!-- .header -->
+
+    </div>
 <div class="text-center">
     <img src="https://www.fipsolutions.de/wp-content/uploads/2018/05/Philipp-Günter-FIPSOLUTIONS.jpg" class="attachment-post-image size-post-image wp-post-image" alt="Philipp Günter - FIPSOLUTIONS Webdesigner in Bonn" srcset="https://www.fipsolutions.de/wp-content/uploads/2018/05/Philipp-Günter-FIPSOLUTIONS.jpg">
     <h1 class="text-center padtop20">FIPSOLUTIONS | seo | web | design aus Bonn</h1>
